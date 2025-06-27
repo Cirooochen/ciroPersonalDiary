52129e882d456a26264b7edafc09c11db7b03f3c
Cirooochen <cirochen0406@gmail.com>
2025-06-27 13:36:09 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello React!</h1>
+    </>
+  );
+}
+
+export default App;
